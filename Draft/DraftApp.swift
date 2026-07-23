6a081d332eeb0b899f04959a6dfbd81303ba1b5a
sameerasw <sameerasw.com@gmail.com>
2026-07-23 19:37:50 +0530

    chore: initial commit

---
+++ Draft/DraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftApp.swift
+//  Draft
+//
+//  Created by Sameera Sandakelum on 2026-07-23.
+//
+
+import SwiftUI
+
+@main
+struct DraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
